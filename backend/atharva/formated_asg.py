c692b7a100728270b1ed8c913f972522a2ebf3e5
Atharva Dhamankar <89476245+semisenioritis@users.noreply.github.com>
2024-07-25 13:53:53 +0000

    asg 2

--- backend/atharva/formated_asg.py
+++ backend/atharva/formated_asg.py
@@ -138,3 +138,94 @@ asg_1 = {
 
 
 
+asg_2 = {
+ 
+ 1:
+  [
+    "A healthcare smartwatch has to be developed for COVID19 patients so that if the device identifies any risks associated with the patient's health condition, it alerts the shift doctors to take preventive measures. Doctors will provide the requirements for this system. For the following given questions identify the type of problem in the given requirement.",
+    {
+        1:[ "A doctor specifies that if the body temperature is more than 104 F, the doctor should be alerted. However, the actual requirement is that if the body temperature is more than 104 F and the oxygen saturation level is less than 92%, then only the device must alert the doctor.", 
+            ["Ambiguous","Inconsistency", "Incompleteness", "None of above"], 
+            ["Incompleteness"] 
+          ],    
+        2:[ "A doctor specifies that if the oxygen saturation level is too low, the device must alert the doctor.", 
+            ["Ambiguous","Inconsistency", "Incompleteness", "None of above"], 
+            ["Ambiguous"] 
+          ], 
+        3:[ "Doctor-1 specifies that if the body temperature is more than 104 F, the doctor should be alerted. Doctor-2 specifies that if the body temperature is more than 103 F and the oxygen saturation level is less than 92%, then only the device must alert the doctor.", 
+            ["Ambiguous","Inconsistency", "Incompleteness", "None of above"], 
+            ["Inconsistency"] 
+          ],                 
+
+    }
+  ],
+ 
+ 
+ 2:
+  [
+    "",
+    {
+        1:[ "Identify the type of requirement collection technique used when it is required to achieve some consensus about the requirements and also highlight the areas of conflict.", 
+            ["Questionnaires","Interviews", "Focus group","Documentations"], 
+            ["Focus group"] 
+          ],    
+
+    }
+  ],
+ 
+ 3:
+  [
+    "",
+    {
+        1:[ "An organisation decided to develop an online medicine selling app. So a team from the organisation visited some medical shops to observe the process of selling the medicines. Identify the type of requirement collection technique used in the given scenario.", 
+            ["Questionnaires", "Focus group","Naturalistic observations","Documentations"], 
+            ["Naturalistic observations"] 
+          ],    
+
+    }
+  ], 
+ 
+  4:
+  [
+    "",
+    {
+        1:[ "A software company has to develop a portal that enables a user to search for a specific product by providing some product details. The portal queries and gathers details from different websites and puts them together on a single page. Consider the following two requirements: Requirement 1: The portal must gather information for the given product from all the relevant websites and display it to the user. Requirement 2: Given the product details, the portal must complete the gathering and display of information within 10 secs. Which of the following statement is true?", 
+            ["Requirement 1 is a functional requirement, whereas Requirement 2 is a non-functional requirement.", "Requirement 2 is a functional requirement, whereas Requirement 1 is a non-functional requirement.","Both requirements are functional requirements.","Both the requirements are non-functional requirements."], 
+            ["Requirement 1 is a functional requirement, whereas Requirement 2 is a non-functional requirement."] 
+          ],    
+
+    }
+  ], 
+ 
+ 5:
+  [
+    "Consider the user story given below for a food delivery app and answer the following questions. Feature 1: Search dishes. As a customer, I want to search for a dish so that I can get the description of the dish along with its price and estimated delivery time within a reasonable time. The results must be displayed with appropriate pagination and filters.",
+    {
+        1:[ "The statement: 'I want to search for a dish' reflects which component of the role-feature-benefit template of the user story?", 
+            ["an action","a benefit", "type of user"], 
+            ["an action"] 
+          ],    
+        2:[ "The statement: 'so that I can get the description of the dish along with its price and estimated delivery time within a reasonable time' reflects which component of the role-featurebenefit template of the user story?", 
+            ["an action","a benefit", "type of user"], 
+            ["a benefit"] 
+          ],  
+        3:[ "Consider the following improvement in the user story. The statement 'As a customer, I want to search for a dish' is modified as: 'As a customer, I want to search for a dish by specifying the dish by name or by the main ingredients used' It makes the user story more ____. ", 
+            ["Specific","Measurable", "Achievable", "Relevant","Timeboxed"], 
+            ["Specific"] 
+          ],  
+        4:[ "Consider the following improvement in the user story. The statement 'so that I can get the description of the dish along with its price and estimated delivery time within a reasonable time. The results must be displayed with appropriate pagination and filters.' is modified as: 'so that I can get the description of the dish along with its price and estimated delivery time within 2 seconds. The results must be displayed with appropriate pagination and filters.' It makes the user story more ____.", 
+            ["Specific","Measurable", "Achievable", "Relevant","Timeboxed"],  
+            ["Measurable"] 
+          ],  
+        5:[ " It has been found that implementation of Feature 1 is not possible within one agile iteration. Feature 1: Search dishes. Thus the Feature 1 is subdivided as follows: Feature 1a: Search and display dishes within a single page. Feature 1b: Add pagination and filters, It makes the user story more ____.", 
+            ["Specific","Measurable", "Achievable", "Relevant","Timeboxed"],  
+            ["Achievable"] 
+          ],  
+        6:[ "The implementation of Feature 1 has been stopped since the time and budget exceeded and the organization has decided to reschedule for the portions left. It makes the user story more ____.", 
+            ["Specific","Measurable", "Achievable", "Relevant","Timeboxed"],   
+            ["Timeboxed"] 
+          ],                                          
+
+    }
+  ],
+}
