35294724ca72b5a5053abb19cc4ea923780e365a
Atharva Dhamankar <89476245+semisenioritis@users.noreply.github.com>
2024-07-25 13:30:32 +0000

    asg1

---
+++ backend/atharva/formated_asg.py
@@ -0,0 +1,140 @@
+asg_1 = {
+    
+ 1:
+  [
+    "",
+    {
+        1:[ "Software can be divided into manageable components. Find the one(s) that are TRUE about breaking software into components from the list below. ", 
+            ["It is a way of breaking the complexity of the system into manageable parts.","It enables different teams to work on different components.", "veryone on the development team must know how to work with all the components.", "It conceals implementation details while providing an interface for others to display its capabilities."], 
+            ["It is a way of breaking the complexity of the system into manageable parts.", "It enables different teams to work on different components.","It conceals implementation details while providing an interface for others to display its capabilities."] 
+          ],    
+
+    }
+  ],
+
+
+
+  2:
+  [
+    "",
+    {
+        1:[ "A software company wants to build a website for employee wellfare to cater services to its own employees, and thus requires to interact with the employees from different departments. In this case which type of clients are we dealing with?", 
+            ["external users","internal users", "software or software components", "developers"], 
+            ["internal users"] 
+          ]
+    }
+  ],
+
+
+  
+
+  3:
+  [
+    "",
+    {
+        1:[ "Presently a lot of software development takes place in a distributed environment, the developer is expected to write precise interface definitions. From the given options, which is/are TRUE about interface definition.", 
+            ["It describes the capabilities of a function.","It provides the implementation details of a function.", "It tells which requests are allowed and specifies the format of the output.", "he code implementing the interface should remain unchanged for a given interface definition."], 
+            ["It describes the capabilities of a function.", "It tells which requests are allowed and specifies the format of the output."] 
+          ]
+    }
+  ],    
+
+
+
+  4:
+  [
+    "",
+    {    
+        1:[ "Which of the following tests ensures that the requirements given by the clients are actually met?", 
+            ["Unit testing","Modular testting", "Integration testing", "Acceptence testing"], 
+            ["Acceptence testing"] 
+          ]
+    }
+  ],
+ 
+
+  5:
+  [
+    "",
+    {    
+        1:[ "The acceptance testing conducted by a group of selected end users in a real-live environment is called _______", 
+            ["Unit testing","Integration testing", "Alpha testing", "Beta testing"], 
+            ["Beta testing"] 
+          ]
+    }
+  ],
+ 
+  6:
+  [
+    "",
+    {    
+        1:[ "Consider X is a software developer working in a renowned software company. X is given responsibility to develop a particular component of a big software. After finishing the coding X wants to test the developed component. Identify the type of testing X wants to perform.", 
+            ["Unit testing","Integration testing", "Alpha testing", "Beta testing"], 
+            ["Unit testing"] 
+          ]
+    }
+  ],
+ 
+  7:
+  [
+    "",
+    {    
+        1:[ "Which of the following are the disadvantages of using waterfall model?", 
+            ["It is not suitable if client requirements are unclear or client is not sure about the entire requirement.","Building prototype results in extra cost which is usually discared afterwards.", "Any change request is made after the requirement gathering phase is completed results in significant increase in cost and time", "The clients gets first view of the product very late compared to other SDLC models."], 
+            ["It is not suitable if client requirements are unclear or client is not sure about the entire requirement.", "Any change request is made after the requirement gathering phase is completed results in significant increase in cost and time", "The clients gets first view of the product very late compared to other SDLC models."] 
+          ]
+    }
+  ],
+ 
+  8:
+  [
+    "",
+    {    
+        1:[ "An EdTech company got a contract to build an E-learning system for a school. It decides to first focus on the key feature that the school requires - i.e. to create an online attendance module. The team develops, tests and delivers this module to the school, and then starts working on the next module. The software development process followed in this case is similar to", 
+            ["Waterfall model","V-model", "Prototype model", "Agile model"], 
+            ["Agile model"] 
+          ]
+    }
+  ],
+
+  9:
+  [
+    "",
+    {    
+        1:[ "Which of the following statements are true?", 
+            ["Description of what your functions can do without describing the implementation in detail is known as an interface.","The code implemented by the interface cannot change once it has been designed.", "The code implemented by the interface can change once it has been designed."], 
+            ["Description of what your functions can do without describing the implementation in detail is known as an interface.","The code implemented by the interface can change once it has been designed."] 
+          ]
+    }
+  ], 
+
+  10:
+  [
+    "A software company wants to develop a website http://www.slightest.com/, that, given a product's details, queries different e-shopping websites and recommends the website that provides the product at the lowest cost. Consider the following tasks, which are performed to build this website, map them to the appropriate phase in the software development process.",
+    {    
+        1:[ "The company designed a set of questions to conduct a survey on how customers want to search for a product and what details they want to provide.", 
+            ["Requirement gathering","Development phase", "Design phase", "Testing phase","Maintenence phase"], 
+            ["Requirement gathering"] 
+          ],
+        2:[ "this is the first question", 
+            ["Requirement gathering","Development phase", "Design phase", "Testing phase","Maintenence phase"],  
+            ["Design phase"] 
+          ],
+        3:[ "this is the first question", 
+            ["Requirement gathering","Development phase", "Design phase", "Testing phase","Maintenence phase"], 
+            ["Development phase"] 
+          ],
+        4:[ "this is the first question", 
+            ["Requirement gathering","Development phase", "Design phase", "Testing phase","Maintenence phase"], 
+            ["Testing phase"] 
+          ],
+        5:[ "this is the first question", 
+            ["Requirement gathering","Development phase", "Design phase", "Testing phase","Maintenence phase"], 
+            ["Maintenence phase"] 
+          ],                                
+    }
+  ],  
+}
+
+
+
